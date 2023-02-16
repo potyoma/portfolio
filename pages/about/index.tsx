2f885ec6ae6e5db968d7e20557631ad8909fca9d
potyoma <artyomnovosyolov254@gmail.com>
2023-02-16 22:32:28 +0400

    feat: template for about page

---
+++ pages/about/index.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image"
+
+const About = () => (
+  <div className="flex flex-col justify-center items-center h-full py-8">
+    <div className="grid grid-cols-6 p-6">
+      <div className="hidden md:block relative w-96 col-span-2">
+        <Image
+          className="object-contain"
+          src="/img/wave.webp"
+          fill
+          alt="Artem's picture"
+        />
+      </div>
+      <div className="border rounded-lg p-4 m-4 col-span-6 md:col-span-4">
+        <h1 className="text-3xl font-semibold">Hi 👋</h1>
+        <h2 className="text-xl font-semibold">
+          My name is Artem and I am the software developer you have been looking
+          for.
+        </h2>
+        <p>have to come up with something</p>
+      </div>
+    </div>
+    <div className="grid grid-cols-6 p-6">
+      <div className="border rounded-lg p-4 m-4 col-span-6 md:col-span-4">
+        <h1 className="text-3xl font-semibold">I mostly work with</h1>
+        <h2 className="text-xl font-semibold">Something about my stack</h2>
+        <p>list of technologies i use</p>
+      </div>
+      <div className="hidden md:block relative w-96 col-span-2">
+        <Image
+          className="object-contain"
+          src="/img/clouds.webp"
+          fill
+          alt="Artem's picture"
+        />
+      </div>
+    </div>
+  </div>
+)
+
+export default About
