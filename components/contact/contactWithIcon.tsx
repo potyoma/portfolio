274cca6ff898d5f86844db21b901613e297d0705
potyoma <artyomnovosyolov254@gmail.com>
2023-02-17 15:30:49 +0400

    fix: better contacts on mobile

--- components/contact/contactWithIcon.tsx
+++ components/contact/contactWithIcon.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const ContactWithIcon: React.FC<Props> = ({ icon, link, children }) => (
   <Link href={link}>
-    <div className="flex gap-5 justify-center items-center hover:text-blue-500">
+    <div className="flex gap-2 md:gap-5 justify-center items-center hover:text-blue-500">
       <FontAwesomeIcon size="2x" icon={icon} />
       <span className="font-bold text-lg">{children}</span>
     </div>
