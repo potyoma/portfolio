0bbfb19a0658bbe024219620626b35c24688a13c
Artyom Novoselov <an@versta24.ru>
2024-01-05 15:58:58 +0100

    feat: footer

--- components/organisms/footer/footer.tsx
+++ components/organisms/footer/footer.tsx
@@ -1,3 +1,9 @@
 export function Footer() {
-  return <></>;
+  const date = new Date();
+
+  return (
+    <footer className="content-block max-w-screen-lg mt-7 flex justify-center text-typography w-[100%] p-3 md:py-7">
+      potyoma | {date.getFullYear()}
+    </footer>
+  );
 }
