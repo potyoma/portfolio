c43198bdc2e248316cf293126980ac54ecbe9e4b
Artyom Novoselov <an@versta24.ru>
2024-01-05 13:04:20 +0100

    feat: extend button styling

--- components/atoms/button/button.tsx
+++ components/atoms/button/button.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
 type ButtonProps = DetailedHTMLProps<
@@ -5,6 +6,13 @@ type ButtonProps = DetailedHTMLProps<
   HTMLButtonElement
 >;
 
-export function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
+export function Button({ children, className, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={clsx("text-typography font-normal", className)}
+    >
+      {children}
+    </button>
+  );
 }
