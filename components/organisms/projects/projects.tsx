ce8782c56a78a9dc22ddf58631dab684232c6d97
Artyom Novoselov <an@versta24.ru>
2024-01-05 14:34:17 +0100

    fix: position center horizontally

--- components/organisms/projects/projects.tsx
+++ components/organisms/projects/projects.tsx
@@ -7,10 +7,10 @@ export function Projects() {
       id="projects"
       className="content-block w-100 p-3 md:py-7 md:px-5 flex flex-col gap-12"
     >
-      <h2 className="uppercase text-typography font-light">
+      <h2 className="uppercase text-typography font-light text-lg">
         Featured projects
       </h2>
-      <ul className="grid sm:grid-cols-2 gap-14">
+      <ul className="grid justify-center sm:grid-cols-2 gap-14">
         {PROJECTS.map(p => (
           <li key={p.name.replace(" ", "-")}>
             <Project project={p} />
