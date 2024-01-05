aec1afa0292336752aa195731a94ea9ad138aa49
Artyom Novoselov <an@versta24.ru>
2024-01-05 13:03:45 +0100

    feat: responsive header

--- components/organisms/header/header.tsx
+++ components/organisms/header/header.tsx
@@ -1,15 +1,15 @@
 import ThemeSwitch from "@/components/molecules/theme-switch";
 import { LINKS } from "@/data/header";
-import Link from "next/link";
+import Link from "@/components/atoms/link";
 
 export function Header() {
   return (
-    <header>
-      <nav className="flex justify-between py-3 px-2">
+    <header className="content-block w-[100%] max-w-screen-lg md:px-5">
+      <nav className="flex justify-between py-7 px-2">
         <Link className="text-typography uppercase" href="/">
           potyoma.
         </Link>
-        <ul className="flex gap-2">
+        <ul className="flex gap-2 md:gap-5">
           {LINKS.map(({ name, href }) => (
             <li key={name}>
               <Link className="text-typography" href={href}>
