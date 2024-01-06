364ad088b7b40d0cd4460c6945495fb341198ea5
Artyom Novoselov <an@versta24.ru>
2024-01-06 21:30:03 +0100

    fix: icon flash

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,17 @@ import "./globals.css";
 import MainLayout from "@/components/templates/main-layout";
 import clsx from "clsx";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "potyoma - Creative Developer",
+  title: "Artem Novoselov - Creative Developer",
   description: "Fullstack Software Developer to build your next website",
 };
 
@@ -18,7 +25,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={clsx(inter.className, "animate-bg flex flex-col items-center py-6")}>
+      <body
+        className={clsx(
+          inter.className,
+          "animate-bg flex flex-col items-center py-6"
+        )}
+      >
         <MainLayout>{children}</MainLayout>
       </body>
     </html>
