5156903a4e48228068f5e0284208f9ba67ecba4c
Artem Novoselov <artyomnovosyolov254@gmail.com>
2024-02-20 13:32:36 +0100

    feat: update hero text

--- data/hero/index.ts
+++ data/hero/index.ts
@@ -1,5 +1,5 @@
 export const INTRODUCTION = {
   intro: "Hey, I'm",
   name: "Artem Novoselov",
-  bio: "I am a passionate and results-driven full-stack developer with a proven track record of delivering high-quality web applications. Proficient in React and Next.js for front-end development, as well as .NET, C# and Node.js for back-end development. I excel in collaborative team environments and thrive on tackling complex challenges with innovative solutions. Also I'm interested in UI/UX design and have knowledge of its principles.",
+  bio: "I offer custom web development services primarily with React and Next. My expertise as a Web Designer, Developer & Frontend Engineer ensures top-notch results for your landing pages, e-commerce sites or web apps. Let's create something amazing together!",
 } as const;
